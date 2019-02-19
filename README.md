965b3780310e88c0f1af072612938e56ed5687a9
mput <putintsev@gmail.com>
2019-02-19 09:25:27 +0300

    Add form validation for url.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # RSS-reader
 [![Build Status](https://travis-ci.org/mput/rss-reader.svg?branch=master)](https://travis-ci.org/mput/rss-reader)
+[![Maintainability](https://api.codeclimate.com/v1/badges/8bbff816987127a2307d/maintainability)](https://codeclimate.com/github/mput/rss-reader/maintainability)
 -------------------------------------
 
 Live at: [rss-hex.surge.sh](http://rss-hex.surge.sh/)
--- src/init.js
+++ src/init.js
@@ -1,8 +1,76 @@
-// @flow
+import validator from 'validator';
+import { watch } from 'melanke-watchjs';
+
+const chenkItput = (url, presentFeeds) => {
+  if (!validator.isURL(url)) {
+    return 'Not valid URL';
+  }
+  if (presentFeeds.find(feed => feed === url)) {
+    return 'URL already exist';
+  }
+  return false;
+};
+
 
 export default () => {
-  const form = document.getElementById('add-url');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
+  const state = {
+    feeds: [],
+    urlForm: {
+      validness: 'unknown', // valid, invalid
+      disabled: false,
+      errorMsg: '',
+    },
+  };
+
+
+  const urlFormHandler = (formElement) => {
+    const urlInput = formElement.querySelector('[name="rss-url"]');
+    const invalidFeedback = formElement.querySelector('.invalid-feedback');
+    const submitBtn = formElement.querySelector('button[type="submit"]');
+
+    watch(state, 'urlForm', () => {
+      submitBtn.disabled = state.urlForm.disabled;
+      invalidFeedback.textContent = state.urlForm.errorMsg;
+      if (state.urlForm.validness === 'unknown') {
+        urlInput.classList.remove('is-invalid');
+        urlInput.classList.remove('is-valid');
+      } else if (state.urlForm.validness === 'invalid') {
+        urlInput.classList.remove('is-valid');
+        urlInput.classList.add('is-invalid');
+      } else if (state.urlForm.validness === 'valid') {
+        urlInput.classList.add('is-valid');
+        urlInput.classList.remove('is-invalid');
+      }
+    });
+
+    urlInput.addEventListener('input', (e) => {
+      const url = e.target.value;
+      if (url === '') {
+        state.urlForm.disabled = true;
+        state.urlForm.validness = 'unknown';
+        state.urlForm.errorMsg = '';
+        return;
+      }
+      const error = chenkItput(url, state.feeds);
+      if (error) {
+        state.urlForm.disabled = true;
+        state.urlForm.validness = 'invalid';
+        state.urlForm.errorMsg = error;
+      } else {
+        state.urlForm.disabled = false;
+        state.urlForm.validness = 'valid';
+        state.urlForm.errorMsg = '';
+      }
+    });
+
+    formElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+      state.feeds = [...state.feeds, urlInput.value];
+      console.log(urlInput.value);
+      urlInput.value = '';
+    });
+  };
+
+  const formElement = document.getElementById('add-url');
+  urlFormHandler(formElement);
 };
