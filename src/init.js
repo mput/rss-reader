6cd6e8dfbf157ce736ff60487dd86498760b4ae5
mput <putintsev@gmail.com>
2019-02-20 00:11:31 +0300

    Fix litn

--- src/init.js
+++ src/init.js
@@ -59,12 +59,12 @@ export default () => {
   });
 
 
-  const articlesCol = document.querySelector('.articles-col')
+  const articlesCol = document.querySelector('.articles-col');
   const feedsList = document.querySelector('.feed');
   const feedTemplate = (title, description, link) => `
   <a class="feed-item list-group-item list-group-item-action" href="${link}">
     <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">${title}</h5>
+      <h6 class="mb-1">${title}</h5>
     </div>
     <p class="mb-1">${description}</p>
   </a>`;
