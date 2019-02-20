cc11a8627d0058ecc723c3ff907276a1958bb3cd
mput <putintsev@gmail.com>
2019-02-20 16:15:52 +0300

    Changed urlFrom state logic.

--- src/init.js
+++ src/init.js
@@ -19,7 +19,7 @@ export default () => {
   const state = {
     feeds: [],
     urlForm: {
-      validness: 'unknown', // valid, invalid
+      state: 'empty', // valid, invalid, waiting
       disabled: false,
       isWaiting: false,
       errorMsg: '',
@@ -37,30 +37,41 @@ export default () => {
   const submitBtnSpinner = submitBtn.querySelector('.spinner-border');
 
   watch(state, 'urlForm', () => {
-    submitBtn.disabled = state.urlForm.disabled;
-    invalidFeedback.textContent = state.urlForm.errorMsg;
+    invalidFeedback.textContent = '';
+    urlInput.classList.remove('is-invalid');
+    urlInput.classList.remove('is-valid');
+    submitBtnSpinner.classList.remove('d-none');
+    urlInput.disabled = false;
+    submitBtn.disabled = false;
 
-    if (state.urlForm.validness === 'unknown') {
-      urlInput.classList.remove('is-invalid');
-      urlInput.classList.remove('is-valid');
-    } else if (state.urlForm.validness === 'invalid') {
-      urlInput.classList.remove('is-valid');
-      urlInput.classList.add('is-invalid');
-    } else if (state.urlForm.validness === 'valid') {
-      urlInput.classList.add('is-valid');
-      urlInput.classList.remove('is-invalid');
-    }
-
-    if (state.urlForm.isWaiting) {
-      submitBtnSpinner.classList.remove('d-none');
-    } else {
-      submitBtnSpinner.classList.add('d-none');
+    switch (state.urlForm.state) {
+      case 'empty':
+        submitBtnSpinner.classList.add('d-none');
+        submitBtn.disabled = true;
+        break;
+      case 'invalid':
+        urlInput.classList.add('is-invalid');
+        submitBtnSpinner.classList.add('d-none');
+        submitBtn.disabled = true;
+        invalidFeedback.textContent = state.urlForm.errorMsg;
+        break;
+      case 'valid':
+        urlInput.classList.add('is-valid');
+        submitBtnSpinner.classList.add('d-none');
+        break;
+      case 'waiting':
+        urlInput.disabled = true;
+        submitBtn.disabled = true;
+        break;
+      default:
     }
   });
 
 
-  const contentPane = document.querySelector('.content-row');
   const feedsList = document.querySelector('.feed');
+  const arcticlesList = document.querySelector('.articles-list');
+  const contentPane = document.querySelector('.content-row');
+
   const feedTemplate = (title, description, link) => `
   <a class="feed-item list-group-item list-group-item-action" href="${link}">
     <div class="d-flex w-100 justify-content-between">
@@ -68,7 +79,6 @@ export default () => {
     </div>
     <p class="mb-1">${description}</p>
   </a>`;
-  const arcticlesList = document.querySelector('.articles-list');
 
   const articleTemplate = (title, link, description) => `
   <li class="list-group-item">
@@ -93,46 +103,37 @@ export default () => {
   urlInput.addEventListener('input', (e) => {
     const url = e.target.value;
     if (url === '') {
-      state.urlForm.disabled = true;
-      state.urlForm.validness = 'unknown';
-      state.urlForm.errorMsg = '';
+      state.urlForm.state = 'empty';
       return;
     }
     const error = chenkItput(url, state.feeds.map(feed => feed.url));
     if (error) {
-      state.urlForm.disabled = true;
-      state.urlForm.validness = 'invalid';
+      state.urlForm.state = 'invalid';
       state.urlForm.errorMsg = error;
     } else {
-      state.urlForm.disabled = false;
-      state.urlForm.validness = 'valid';
-      state.urlForm.errorMsg = '';
+      state.urlForm.state = 'valid';
     }
   });
 
   formElement.addEventListener('submit', async (e) => {
     e.preventDefault();
-    state.urlForm.disabled = true;
-    state.urlForm.isWaiting = true;
+    state.urlForm.state = 'waiting';
 
     const feedUrl = urlInput.value;
     try {
       const feed = new RssFeed(feedUrl);
       await feed.update();
       state.feeds = [...state.feeds, feed];
-      state.urlForm.validness = 'unknown';
+      state.urlForm.state = 'empty';
       urlInput.value = '';
     } catch (error) {
-      state.urlForm.validness = 'invalid';
+      state.urlForm.state = 'invalid';
       state.urlForm.errorMsg = 'Something wrong with this feed';
     }
-    state.urlForm.disabled = false;
-    state.urlForm.isWaiting = false;
   });
 
-  $('#modal').on('show.bs.modal', function (event) {
+  $('#modal').on('show.bs.modal', (event) => {
     const button = $(event.relatedTarget);
-    const recipient = button.data('whatever') // Extract info from data-* attributes
     const title = button.data('title');
     const description = button.data('description');
     const href = button.data('href');
@@ -141,5 +142,4 @@ export default () => {
     modal.find('.description').text(description);
     modal.find('.open-link').attr('href', href);
   });
-
 };
